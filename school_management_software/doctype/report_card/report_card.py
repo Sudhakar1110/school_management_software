377e51c0c89325d38116f58ba2e82a0d44f54275
Sujai <sujaiit0696@gmail.com>
2026-07-17 10:28:32 +0530

    feat: Add Python modules for ALL old DocTypes (93 DocTypes now have doctype/ folders)

---
+++ school_management_software/doctype/report_card/report_card.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2026, School Management and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+class ReportCard(Document):
+    pass
